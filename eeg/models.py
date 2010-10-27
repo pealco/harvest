689de6c1fe4bf0ad48e691c54e35c40bb9308353
Pedro Alcocer <pealco@gmail.com>
2010-10-27 10:15:19 -0400

    Changes.

--- eeg/models.py
+++ eeg/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
 class Experiment(models.Model):
-    name = models.CharField(max_length=100)
+    name        = models.CharField(max_length=100)
 
 class Subject(models.Model):
-    name       = models.CharField(blank=True, max_length=100)
-    subject_id = models.CharField(blank=True, max_length=100)
-    experiment = models.ForeignKey(Experiment)
+    name        = models.CharField(blank=True, max_length=100)
+    subject_id  = models.CharField(blank=True, max_length=100)
+    experiment  = models.ForeignKey(Experiment)
+    path        = models.FilePathField(path="/Users/pealco/archive/data")
 
 class Epoch(models.Model):
-    trigger         = models.IntegerField()
-    pre_trigger     = models.IntegerField()
-    post_trigger    = models.IntegerField()
-    electrode       = models.CharField(max_length=4)
-    condition       = models.CharField(max_length=100)
-    word_num        = models.PositiveIntegerField()
-    subject         = models.ForeignKey(Subject)
+    trigger     = models.IntegerField()
+    condition   = models.CharField(max_length=100)
+    word_num    = models.PositiveIntegerField()
+    subject     = models.ForeignKey(Subject)
 
 class Measurement(models.Model):
-    value   = models.DecimalField(max_digits=10, decimal_places=10)
-    index   = models.IntegerField()
-    epoch   = models.ForeignKey(Epoch)
+    value       = models.DecimalField(max_digits=10, decimal_places=10)
+    index       = models.IntegerField()
+    subject     = models.ForeignKey(Subject)
+    experiment  = models.ForeignKey(Experiment)
+    electrode   = models.CharField(max_length=4)
--- eeg/views.py
+++ eeg/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+
+def load_data(request):
+    if request.method == 'POST':
+        form = DataLoadForm(request.POST)
+        if form.is_valid():
+            
+            data_path = form.cleaned_data['data_path']
+        
